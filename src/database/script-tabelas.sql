0f6a7de2fc8e871621f990d588f0c591ccdc19c9
Bianca-LOrtega <bianca.ortega@sptech.school>
2025-05-19 13:31:41 -0300

    modelagem de dados

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,30 +1,16 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+-- modelo de dados --Diario Winchester
 
-/*
-comandos para mysql server
-*/
+create database diario;
+use diario;
 
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
-);
-
-CREATE TABLE usuario (
+create table usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(50),
 	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
 );
+
+-------------------------------
 
 CREATE TABLE aviso (
 	id INT PRIMARY KEY AUTO_INCREMENT,
