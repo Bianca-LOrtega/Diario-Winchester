cb2ecbbc820eff243166c5346c93b613ee799066
Bianca-LOrtega <bianca.ortega@sptech.school>
2025-05-21 15:07:02 -0300

    testes api

--- src/models/respQuizModel.js
+++ src/models/respQuizModel.js
@@ -1,14 +1,22 @@
 var database = require("../database/config");
 
-function registrarResposta(fkusuario, fkpergunta, acerto) {
+function registrarResposta(idUsuario, fkpergunta, acerto) {
     var instrucao = `
         INSERT INTO resp_quiz (fkusuario, fkpergunta, acerto) 
-        VALUES (${fkusuario}, ${fkpergunta}, ${acerto});
+        VALUES (${idUsuario}, ${fkpergunta}, ${acerto});
     `;
     console.log("Executando SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
 
+function pontuacao() {
+    var instrucao = ` SELECT fkusuario, COUNT(*) AS qtd FROM resp_quiz
+                        WHERE acertos = 1
+                        GROUP BY fkusuario;
+                    `
+}
+
 module.exports = {
-    registrarResposta
+    registrarResposta,
+    pontuacao
 };
